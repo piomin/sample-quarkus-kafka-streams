64626a859bd6bc40118b283ac88dff7aefac7b94
piomin <piotr.minkowski@gmail.com>
2023-05-04 01:10:00 +0200

    refactoring

--- stock-service/src/test/java/pl/piomin/samples/streams/order/StockServiceAppTests.java
+++ stock-service/src/test/java/pl/piomin/samples/streams/order/StockServiceAppTests.java
@@ -3,11 +3,15 @@ package pl.piomin.samples.streams.order;
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.given;
+
 @QuarkusTest
 public class StockServiceAppTests {
 
     @Test
     void startup() {
-
+        given().get("/q/health")
+                .then()
+                .statusCode(503);
     }
 }
